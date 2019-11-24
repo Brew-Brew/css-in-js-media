16a82d9dc1217b279e440eb70980dc2bec38ceea
NoMoreViolence <ljh86029926@gmail.com>
2019-11-24 22:23:00 +0900

    feat: type definition for typescript

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for css-in-js-media
+// Project: https://github.com/zx6658/css-in-js-media 
+// Definitions by:  Ji-Hoon Lee <https://github.com/NoMoreViolence>
+// TypeScript Version: 3.7.2
+
+declare module 'css-in-js-media' {
+  declare function cssInJsMedia(query?: string, query?: string): string
+  export = cssInJsMedia
+}
